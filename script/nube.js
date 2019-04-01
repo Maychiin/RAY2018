cb85d435247f7de295d46f1b2f505456ae041efb
May <marynesduque@hotmail.com>
2019-04-01 21:23:43 +0100

    Fixed

--- script/nube.js
+++ script/nube.js
@@ -15,15 +15,13 @@ var footerBackColor = "#24248f";
 var headerTextColor = "white";
 var bodyTextColor = "black";
 
+
 var currentPanel = 0;
 
 
 function initGame() {
 	game_canvas = document.getElementById("gameCanvas");
 	game_context = game_canvas.getContext("2d");
-}
-
-function updateGame() {
     
     if (!game_loaded) {
         
@@ -36,6 +34,7 @@ function updateGame() {
     }
     
 
+    
     console.log("juego cargado");
     
     cloudx = game_canvas.width;
@@ -393,4 +392,43 @@ function drawText (posx, posy ,text, fontColor) {
 }
 
 
+
+/* function updateGame() {
+	
+	fechaActual = new Date();
+	
+	var tiempoTranscurrido = fechaActual - lastTime;
+	var fpsActuales = Math.round(1000 / tiempoTranscurrido);
+	
+	game_context.clearRect(0, 0, game_canvas.width, game_canvas.height);
+
+	//game_context.fillStyle = "red"; //Color del "pincel" con el que se pinta.
+	//game_context.fillRect(50, 50, 100, 100);
+	//(x, y, Ancho, largo)
+
+	game_context.fillStyle = "red";
+	game_context.beginPath();
+	game_context.arc(100, 100, 50, 0, 2 * Math.PI);
+	game_context.fill();
+
+	game_context.fillStyle = "pink";
+	cloudx = cloudx - 3;
+	game_context.fillRect(cloudx, 50, 100, 100);
+    
+	game_context.fillStyle = "black";
+	game_context.fillRect(game_canvas.width - cloudx, 50, 100, 100);
+
+	game_context.fillStyle = "yellow";
+	game_context.fillRect(game_canvas.width - 75, 0, 75, 30);
+
+	game_context.fillStyle = "black";
+	game_context.font = "16px Arial";
+	game_context.fillText(fpsActuales, game_canvas.width - 75, 15);
+	
+	
+	fechaActual = new Date();
+	lastTime = fechaActual.getTime();
+}
+
+initGame(); */
 
